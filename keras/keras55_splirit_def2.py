201f7cf2d063b9669469f3006fad4de10c4e4e1b
G-sup <76672819+Jisup-lim@users.noreply.github.com>
2021-01-19 21:03:29 +0900

    1월18일 수업

--- keras/keras55_splirit_def2.py
+++ keras/keras55_splirit_def2.py
@@ -10,7 +10,7 @@ def split_x(D,size,y_cols):
     x , y = [] , []
     for i in range(len(D)):
         x_end_number = i + size
-        y_end_number = x_end_number + y_cols
+        y_end_number = x_end_number + y_cols 
         if y_end_number > len(D) :
             break
         tem_x = D[i : x_end_number,:]
@@ -19,7 +19,7 @@ def split_x(D,size,y_cols):
         y.append(tem_y)
     return np.array(x),np.array(y)
     
-x, y = split_x(D,4,1)
+x, y = split_x(D,3,1)
 
 print(x)
 print('----------------')
@@ -31,35 +31,3 @@ print(y.shape)
 
 
 
-'''
-
-import numpy as np
-
-D = np.array([range(1,11),range(11,21),range(21,31),range(31,41)])
-D = D.T
-
-print(D)
-
-def split_x(D,x_row,x_cols,y_row,y_cols):
-    x , y = [] , []
-    for i in range(len(D)):
-        x_end_number = i + x_row
-        y_end_number = x_end_number + y_cols
-        if y_end_number > len(D) :
-            break
-        tem_x = D[i : x_end_number,:]
-        tem_y = D[x_end_number:y_end_number, :]  # 뒤 숫자에 따라 y가 변한다 -1 = (1개씩), : = (한 행)
-        x.append(tem_x)
-        y.append(tem_y)
-    return np.array(x),np.array(y)
-    
-x, y = split_x(D,4,1)
-
-print(x)
-print('----------------')
-print(y)
-print(y.shape)
-
-
-# 한면에서 한열을 확인
-'''
