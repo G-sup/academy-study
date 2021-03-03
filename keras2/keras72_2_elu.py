9adfe52d9416dd6877d02ea46757f6ac70ff5447
G-sup <76672819+Jisup-lim@users.noreply.github.com>
2021-03-03 09:29:31 +0900

    3월 2일 수업

--- keras2/keras72_2_elu.py
+++ keras2/keras72_2_elu.py
@@ -1,19 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def relu(x) :
-    return np.maximum(0, x)
+alpa = 0.5
+def elu(x) :
+    # return  np.maximum(2*(np.exp(x)-1) * abs(x)/-x, x) # relu와 유사함, 입력이 0 이하일 경우 부드럽게 깎아준다.
+    return (x>=0)*x + (x<0)*alpa*(np.exp(x)-1)  # 0보다 크면 x를 반환, 0보다 작으면 뒤의 계산식 반환
 
 x = np.arange(-5, 5, 0.1)
-y = relu(x)
+y = elu(x)
 
 print(x)
 print(y)
 
-plt.plot(x,y)
+# 시각화
+plt.plot(x, y)
 plt.grid()
 plt.show()
-
-# 과제
-# elu, selu, leaky relu
-# 72_2,3,4
--- keras2/keras72_3_selu.py
+++ keras2/keras72_3_selu.py
@@ -1,19 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def relu(x) :
-    return np.maximum(0, x)
+scale = 1.0507
+alpha = 1.6733
+
+def selu(x) :
+    return scale * (np.maximum(0,x)+np.minimum(0,alpha*(np.exp(x)-1)))
+
+# np.maximum : 두 인자 중에서 더 큰 값을 반환한다.
+# np.minimum : 두 인자 중에서 더 작은 값을 반환한다.
 
 x = np.arange(-5, 5, 0.1)
-y = relu(x)
+y = selu(x)
 
 print(x)
 print(y)
 
-plt.plot(x,y)
+# 시각화
+plt.plot(x, y)
 plt.grid()
 plt.show()
 
-# 과제
-# elu, selu, leaky relu
-# 72_2,3,4
+# https://pytorch.org/docs/stable/generated/torch.nn.SELU.html
