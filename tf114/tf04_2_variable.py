b10a64331ccd85b8ba60cc31cf9a2c7ad9a6f8c0
G-sup <76672819+Jisup-lim@users.noreply.github.com>
2021-03-10 10:00:30 +0900

    3월9일 수업

--- tf114/tf04_2_variable.py
+++ tf114/tf04_2_variable.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 sess = tf.Session()
 
 
-x= tf.Variable([2], dtype=tf.float32, name = 'test')  # Variable 변수 가지 자료형 중에 하나
+x= tf.Variable([2], dtype=tf.float32, name = 'test')  # Variable 변수 가지 자료형 중에 하나. 연산의 개념
 
 init = tf.global_variables_initializer() # Variable 은 sess.run을 통과전에 무조건 초기화 해야한다 
 # 값이 바뀌는건 아니다 한번만 선언 해주면된다(텐서플로우에 사용하기위해)
